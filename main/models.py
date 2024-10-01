25e96a05f808185257e27e115c14a26238eb2bf2
WendyFyfo <wendyfyfo@gmail.com>
2024-10-02 06:01:03 +0700

    Fix header and footer

--- main/models.py
+++ main/models.py
@@ -8,4 +8,4 @@ class ProductEntry(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
-    image = models.ImageField(upload_to='product_images/',blank=False, null=False)
+    image = models.ImageField(upload_to='product_images/',blank=False, null=False, default='static/assets/image/image-not-found.png')
